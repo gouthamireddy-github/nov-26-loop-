8c5840ba30813c3931a9ceaa65adf241c4805fdf
gouthamireddy-github <gouthamireddyvelma@gmail.com>
2024-11-26 21:42:14 +0530

    for loop and array

---
+++ index.js
@@ -0,0 +1,36 @@
+for(var i=1 ; i<=20 ; i++){
+    console.log(`number count is ${i}`);
+}
+console.log("-----------------------------------");
+
+for(var i=1 ; i<=10 ; i++){
+    console.log(`saquare of ${i} is` ,i*i);
+}
+console.log("-----------------------------------");
+
+for(var i=1 ; i<=10 ; i++){
+    console.log(`5 * ${i} =`,5*i);
+}
+console.log("-----------------------------------");
+
+for(var i=20 ; i>=1 ; i--){
+    console.log(i);
+}
+console.log("-----------------------------------");
+
+for(var i=20 ; i>=1 ; i--){
+    if(i%2==0)   
+    console.log(i)
+}
+console.log("-----------------------------------");
+
+array=[10,20,30,40,50];
+for(var i=0 ; i<=array.length-1;i++){
+    console.log(array[i]);
+}
+console.log("----------------------------------------");
+
+var arr=['apple', 'banana', 'cherry'] 
+console.log(`index ${arr.indexOf('apple')} : apple`);
+console.log(`index ${arr.indexOf('apple')} : banana`);
+console.log(`index ${arr.indexOf('cherry')} : cherry`);
